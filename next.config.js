5cbecbe7e743ce4d0c05a8df92355044aeca0ccc
Gulrez <sidd5312@gmailcom>
2021-12-03 05:29:16 +0530

    initial

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    images: {
+        domains: ["links.papareact.com"],
+    },
+    env: {
+        mapbox_key: 'pk.eyJ1Ijoic2lkZDUzMTIiLCJhIjoiY2t3cGp3MXR3MGRkaDJ3cWt3dzllbnkwMCJ9.Bx989Jut8V05d8SbOul3-g'
+    }
+}
